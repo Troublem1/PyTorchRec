be4d4208a26227e0470135cb5de8b818552f5b0f
阎世宏 <yansh19@mails.tsinghua.edu.cn>
2020-05-06 14:59:08 +0800

    update CrossedColumn

--- torchrec/feature_columns/crossed_column.py
+++ torchrec/feature_columns/crossed_column.py
@@ -3,21 +3,25 @@
 """
 from typing import Dict, Any, List
 
-from torch import LongTensor
+from torch import Tensor
 
 from .categorical_column import CategoricalColumn
 
 
-class CrossedColumn:
+class CrossedColumn(CategoricalColumn):
     """分类特征交叉列，输入必须是已经创建好的分类特征列"""
 
     def __init__(self, categorical_columns: List[CategoricalColumn]):
+        category_num = 1
+        for categorical_column in categorical_columns:
+            category_num *= categorical_column.category_num
+        super().__init__(category_num)
         self.categorical_columns = categorical_columns
         self.coefficients = [1] * len(categorical_columns)
         for i in range(len(categorical_columns) - 1, 0, -1):
             self.coefficients[i - 1] = self.coefficients[i] * self.categorical_columns[i].category_num
 
-    def get_feature_data(self, batch: Dict[str, Any]) -> LongTensor:
+    def get_feature_data(self, batch: Dict[str, Any]) -> Tensor:
         """获取特征数据"""
-        return sum((self.coefficients[i] * batch[self.categorical_columns[i].feature_name].long()
-                    for i in range(len(self.coefficients))))
+        return sum((self.coefficients[i] * self.categorical_columns[i].get_feature_data(batch)
+                    for i in range(len(self.categorical_columns))))
