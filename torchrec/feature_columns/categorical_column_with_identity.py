68ba052597ade9ff986df76a33bf0d96854ac747
阎世宏 <yansh19@mails.tsinghua.edu.cn>
2020-05-06 14:46:04 +0800

    add CategoricalColumnWithIdentity

---
+++ torchrec/feature_columns/categorical_column_with_identity.py
@@ -0,0 +1,37 @@
+"""
+离散分类特征类
+"""
+from typing import Dict, Any
+
+from pandas import Series
+from pandas.api import types
+from torch import LongTensor
+
+from torchrec.utils.const import *
+from .categorical_column import CategoricalColumn
+
+
+class CategoricalColumnWithIdentity(CategoricalColumn):
+    """离散分类特征类，输入必须是整数，输入不建议过于稀疏，等价于生成[0,category_num)的one-hot向量"""
+
+    def __init__(self, category_num: int, feature_name: str):
+        super().__init__(category_num)
+        self.feature_name = feature_name
+
+    def get_feature_data(self, batch: Dict[str, Any]) -> LongTensor:
+        """获取特征数据"""
+        return batch[self.feature_name].long()
+
+    @staticmethod
+    def from_series(feature_name: str, series: Series, other_info: Dict[str, Any]):
+        """从pandas.Series中构造"""
+        assert types.is_integer_dtype(series), series.dtypes
+        column = CategoricalColumnWithIdentity(
+            feature_name=feature_name,
+            category_num=series.max() + 1
+        )
+        column.set_info(MIN, series.min())
+        column.set_info(MAX, series.max())
+        for key, value in other_info.items():
+            column.set_info(key, value)
+        return column
