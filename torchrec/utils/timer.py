cd5b9589f198ca3e2851edfbe437d5f8132b7fe9
阎世宏 <yansh19@mails.tsinghua.edu.cn>
2020-05-08 00:31:58 +0800

    update timer: divided_by

--- torchrec/utils/timer.py
+++ torchrec/utils/timer.py
@@ -5,12 +5,13 @@ from time import time
 class Timer:
     """时间统计上下文管理"""
 
-    def __init__(self):
+    def __init__(self, divided_by: int = 1):
         self.time = None
+        self.divided_by = divided_by
 
     def __enter__(self):
         self.time = time()
-        return self.time
+        # return self.time
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print('时间：%.3f' % (time() - self.time))
+        print('时间：%.3f' % ((time() - self.time) / self.divided_by))
