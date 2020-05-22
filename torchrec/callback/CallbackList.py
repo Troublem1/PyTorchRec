c8a022bdb850e0e7524a060c1b2de4043def32dd
阎世宏 <yansh19@mails.tsinghua.edu.cn>
2020-05-23 00:26:05 +0800

    add performance fix: CallbackList

--- torchrec/callback/CallbackList.py
+++ torchrec/callback/CallbackList.py
@@ -1,7 +1,12 @@
 """
 回调列表
 """
-from typing import List, Optional, Dict
+import collections
+import logging
+import time
+from typing import List
+
+import numpy as np
 
 from torchrec.callback.History import History
 from torchrec.callback.ICallback import ICallback
@@ -9,32 +14,57 @@ from torchrec.callback.ProgbarLogger import ProgbarLogger
 from torchrec.utils.data_structure import tensor_to_numpy_or_python_type
 
 
+class ModeKeys:
+    TRAIN = 'train'
+    TEST = 'test'
+    PREDICT = 'predict'
+
+
 class CallbackList:
     """回调列表"""
 
     def __init__(self,
-                 callbacks: Optional[List[ICallback]] = None,
+                 callbacks: List[ICallback] = None,
                  add_history: bool = False,
                  add_progbar: bool = False,
-                 recommender=None,
+                 model=None,
                  **params):
         """
-        创建回调函数列表
-        :param callbacks: 回调对象列表
-        :param add_history: 是否添加History
-        :param add_progbar: 是否添加ProgbarLogger
-        :param recommender: 推荐算法引用
-        :param params: 其他要传递给ICallback.set_params的参数
+        :param callbacks: 回调实例列表
+        :param add_history: 是否增加历史回调
+        :param add_progbar: 是否增加进度条回调
+        :param model: 模型实例
+        :param params: 其他参数
         """
-        self.callbacks: List[ICallback] = callbacks or []
+        self.callbacks = callbacks or []
+        self._add_default_callbacks(add_history, add_progbar)
+        self.model = None
+        self.params = None
+
+        if model:
+            self.set_model(model)
+        if params:
+            self.set_params(params)
+
+        self._queue_length = 10
+        self._reset_batch_timing()
+
+        self._should_call_train_batch_hooks = any(
+            cb.implements_train_batch_hooks() for cb in self.callbacks)
+        self._should_call_test_batch_hooks = any(
+            cb.implements_test_batch_hooks() for cb in self.callbacks)
+        self._should_call_predict_batch_hooks = any(
+            cb.implements_predict_batch_hooks() for cb in self.callbacks)
+
+    def _add_default_callbacks(self, add_history: bool, add_progbar: bool):
         self._progbar = None
         self._history = None
 
-        for callback in self.callbacks:
-            if isinstance(callback, ProgbarLogger):
-                self._progbar = callback
-            elif isinstance(callback, History):
-                self._history = callback
+        for cb in self.callbacks:
+            if isinstance(cb, ProgbarLogger):
+                self._progbar = cb
+            elif isinstance(cb, History):
+                self._history = cb
 
         if self._progbar is None and add_progbar:
             self._progbar = ProgbarLogger()
@@ -44,111 +74,153 @@ class CallbackList:
             self._history = History()
             self.callbacks.append(self._history)
 
-        if recommender:
-            self.recommender = recommender
-            if self._history:
-                recommender.history = self._history
-            for callback in self.callbacks:
-                callback.set_recommender(recommender)
-        if params:
-            self.params = params
-            for callback in self.callbacks:
-                callback.set_params(params)
+    def _reset_batch_timing(self):
+        self._delta_t_batch = 0.
+        self._delta_ts = collections.defaultdict(
+            lambda: collections.deque([], maxlen=self._queue_length))
 
-        # todo 根据回调是否实现了batch级别的回调，来决定是否调用（优化性能）
-        # todo 根据回调时间判断回调占时与batch占时比例，避免回调严重拖慢速度
-
-    @staticmethod
-    def _process_logs(logs: Optional[Dict]):
-        """将tensor转化为ndarray或者标量数据"""
+    def _process_logs(self, logs):
+        """Turns tensors into numpy arrays or Python scalars."""
         if logs:
             return tensor_to_numpy_or_python_type(logs)
         return {}
 
-    def append(self, callback: ICallback):
-        """添加回调"""
+    def append(self, callback):
         self.callbacks.append(callback)
 
-    def on_epoch_begin(self, epoch: int, logs: Optional[Dict] = None):
-        """轮次开始"""
+    def set_params(self, params):
+        self.params = params
+        for cb in self.callbacks:
+            cb.set_params(params)
+
+    def set_model(self, model):
+        self.model = model
+        if self._history:
+            model.history = self._history
+        for cb in self.callbacks:
+            cb.set_model(model)
+
+    def _call_batch_hook(self, mode, hook, batch, logs=None):
+        """Helper function for all batch_{begin | end} methods."""
+        if not self.callbacks:
+            return
+        hook_name = 'on_{mode}_batch_{hook}'.format(mode=mode, hook=hook)
+        if hook == 'begin':
+            self._t_enter_batch = time.time()
+        if hook == 'end':
+            # Batch is ending, calculate batch time.
+            self._delta_t_batch = time.time() - self._t_enter_batch
+
+        logs = logs or {}
+        t_before_callbacks = time.time()
+        for callback in self.callbacks:
+            batch_hook = getattr(callback, hook_name)
+            batch_hook(batch, logs)
+        self._delta_ts[hook_name].append(time.time() - t_before_callbacks)
+
+        delta_t_median = np.median(self._delta_ts[hook_name])
+        if (self._delta_t_batch > 0. and
+                delta_t_median > 0.95 * self._delta_t_batch and delta_t_median > 0.1):
+            logging.warning(
+                'Method (%s) is slow compared '
+                'to the batch update (%f). Check your callbacks.', hook_name,
+                delta_t_median)
+
+    def _call_begin_hook(self, mode):
+        """Helper function for on_{train|test|predict}_begin methods."""
+        if mode == ModeKeys.TRAIN:
+            self.on_train_begin()
+        elif mode == ModeKeys.TEST:
+            self.on_test_begin()
+        else:
+            self.on_predict_begin()
+
+    def _call_end_hook(self, mode):
+        """Helper function for on_{train|test|predict}_end methods."""
+        if mode == ModeKeys.TRAIN:
+            self.on_train_end()
+        elif mode == ModeKeys.TEST:
+            self.on_test_end()
+        else:
+            self.on_predict_end()
+
+    def on_batch_begin(self, batch, logs=None):
+        if self._should_call_train_batch_hooks:
+            logs = self._process_logs(logs)
+            self._call_batch_hook(ModeKeys.TRAIN, 'begin', batch, logs=logs)
+
+    def on_batch_end(self, batch, logs=None):
+        if self._should_call_train_batch_hooks:
+            logs = self._process_logs(logs)
+            self._call_batch_hook(ModeKeys.TRAIN, 'end', batch, logs=logs)
+
+    def on_epoch_begin(self, epoch, logs=None):
         logs = self._process_logs(logs)
         for callback in self.callbacks:
             callback.on_epoch_begin(epoch, logs)
+        self._reset_batch_timing()
 
-    def on_epoch_end(self, epoch: int, logs: Optional[Dict] = None):
-        """轮次结束"""
+    def on_epoch_end(self, epoch, logs=None):
         logs = self._process_logs(logs)
         for callback in self.callbacks:
             callback.on_epoch_end(epoch, logs)
 
-    def on_train_batch_begin(self, batch: int, logs: Optional[Dict] = None):
-        """训练批次开始"""
-        logs = self._process_logs(logs)
-        for callback in self.callbacks:
-            callback.on_train_batch_begin(batch, logs)
+    def on_train_batch_begin(self, batch, logs=None):
+        if self._should_call_train_batch_hooks:
+            logs = self._process_logs(logs)
+            self._call_batch_hook(ModeKeys.TRAIN, 'begin', batch, logs=logs)
 
-    def on_train_batch_end(self, batch: int, logs: Optional[Dict] = None):
-        """训练批次结束"""
-        logs = self._process_logs(logs)
-        for callback in self.callbacks:
-            callback.on_train_batch_end(batch, logs)
+    def on_train_batch_end(self, batch, logs=None):
+        if self._should_call_train_batch_hooks:
+            logs = self._process_logs(logs)
+            self._call_batch_hook(ModeKeys.TRAIN, 'end', batch, logs=logs)
 
-    def on_test_batch_begin(self, batch: int, logs: Optional[Dict] = None):
-        """验证/测试批次开始"""
-        logs = self._process_logs(logs)
-        for callback in self.callbacks:
-            callback.on_test_batch_begin(batch, logs)
+    def on_test_batch_begin(self, batch, logs=None):
+        if self._should_call_test_batch_hooks:
+            logs = self._process_logs(logs)
+            self._call_batch_hook(ModeKeys.TEST, 'begin', batch, logs=logs)
 
-    def on_test_batch_end(self, batch: int, logs: Optional[Dict] = None):
-        """验证/测试批次结束"""
-        logs = self._process_logs(logs)
-        for callback in self.callbacks:
-            callback.on_test_batch_end(batch, logs)
+    def on_test_batch_end(self, batch, logs=None):
+        if self._should_call_test_batch_hooks:
+            logs = self._process_logs(logs)
+            self._call_batch_hook(ModeKeys.TEST, 'end', batch, logs=logs)
 
-    def on_predict_batch_begin(self, batch: int, logs: Optional[Dict] = None):
-        """预测批次开始"""
-        logs = self._process_logs(logs)
-        for callback in self.callbacks:
-            callback.on_predict_batch_begin(batch, logs)
+    def on_predict_batch_begin(self, batch, logs=None):
+        if self._should_call_predict_batch_hooks:
+            logs = self._process_logs(logs)
+            self._call_batch_hook(ModeKeys.PREDICT, 'begin', batch, logs=logs)
 
-    def on_predict_batch_end(self, batch: int, logs: Optional[Dict] = None):
-        """预测批次结束"""
-        logs = self._process_logs(logs)
-        for callback in self.callbacks:
-            callback.on_predict_batch_end(batch, logs)
+    def on_predict_batch_end(self, batch, logs=None):
+        if self._should_call_predict_batch_hooks:
+            logs = self._process_logs(logs)
+            self._call_batch_hook(ModeKeys.PREDICT, 'end', batch, logs=logs)
 
-    def on_train_begin(self, logs: Optional[Dict] = None):
-        """训练开始"""
+    def on_train_begin(self, logs=None):
         logs = self._process_logs(logs)
         for callback in self.callbacks:
             callback.on_train_begin(logs)
 
-    def on_train_end(self, logs: Optional[Dict] = None):
-        """训练结束"""
+    def on_train_end(self, logs=None):
         logs = self._process_logs(logs)
         for callback in self.callbacks:
             callback.on_train_end(logs)
 
-    def on_test_begin(self, logs: Optional[Dict] = None):
-        """验证/测试开始"""
+    def on_test_begin(self, logs=None):
         logs = self._process_logs(logs)
         for callback in self.callbacks:
             callback.on_test_begin(logs)
 
-    def on_test_end(self, logs: Optional[Dict] = None):
-        """验证/测试结束"""
+    def on_test_end(self, logs=None):
         logs = self._process_logs(logs)
         for callback in self.callbacks:
             callback.on_test_end(logs)
 
-    def on_predict_begin(self, logs: Optional[Dict] = None):
-        """预测开始"""
+    def on_predict_begin(self, logs=None):
         logs = self._process_logs(logs)
         for callback in self.callbacks:
             callback.on_predict_begin(logs)
 
-    def on_predict_end(self, logs: Optional[Dict] = None):
-        """预测结束"""
+    def on_predict_end(self, logs=None):
         logs = self._process_logs(logs)
         for callback in self.callbacks:
             callback.on_predict_end(logs)
