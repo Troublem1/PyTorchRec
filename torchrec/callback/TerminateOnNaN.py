3f5f9c6b18756951ce33af2b02d3c4cf5b96087a
阎世宏 <yansh19@mails.tsinghua.edu.cn>
2020-05-22 17:02:46 +0800

    add callback.TerminateOnNaN

---
+++ torchrec/callback/TerminateOnNaN.py
@@ -0,0 +1,20 @@
+"""
+当损失为异常值时终止训练
+"""
+from typing import Optional, Dict
+
+import numpy as np
+
+from torchrec.callback.ICallback import ICallback
+
+
+class TerminateOnNaN(ICallback):
+    """当损失为异常值时终止训练"""
+
+    def on_batch_end(self, batch: int, logs: Optional[Dict] = None):
+        logs = logs or {}
+        loss = logs.get('loss')
+        if loss is not None:
+            if np.isnan(loss) or np.isinf(loss):
+                print(f'Batch {batch}: Invalid loss, terminating training')
+                self.model.stop_training = True
