4b339da719f7fb97bd830042b4fcc7b5673c3778
阎世宏 <yansh19@mails.tsinghua.edu.cn>
2020-05-22 16:54:54 +0800

    add callback.ICallback.empty_implementation

--- torchrec/callback/ICallback.py
+++ torchrec/callback/ICallback.py
@@ -2,23 +2,29 @@
 回调函数接口类
 """
 from abc import ABC
-from typing import Optional, Dict
+from typing import Optional, Dict, Callable
+
+
+def empty_implementation(method: Callable):
+    """标明一个函数的实现没有任何语句，可以忽略，通常为基类函数"""
+    method._empty_implementation = True
+    return method
 
 
 class ICallback(ABC):
     """回调函数接口类"""
 
     def __init__(self):
-        self.recommender = None
+        self.model = None
         self.params: Optional[Dict] = None
 
     def set_params(self, params):
         """设置参数"""
         self.params = params
 
-    def set_recommender(self, recommender):
+    def set_model(self, model):
         """引用推荐模型"""
-        self.recommender = recommender
+        self.model = model
 
     def on_epoch_begin(self, epoch: int, logs: Optional[Dict] = None):
         """轮次开始时调用"""
@@ -28,26 +34,32 @@ class ICallback(ABC):
         """轮次结束时调用"""
         pass
 
+    @empty_implementation
     def on_train_batch_begin(self, batch: int, logs: Optional[Dict] = None):
         """训练批次开始时调用"""
         pass
 
+    @empty_implementation
     def on_train_batch_end(self, batch: int, logs: Optional[Dict] = None):
         """训练批次结束后调用"""
         pass
 
+    @empty_implementation
     def on_test_batch_begin(self, batch: int, logs: Optional[Dict] = None):
         """验证/测试批次开始的时候"""
         pass
 
+    @empty_implementation
     def on_test_batch_end(self, batch: int, logs: Optional[Dict] = None):
         """验证/测试批次结束的时候"""
         pass
 
+    @empty_implementation
     def on_predict_batch_begin(self, batch: int, logs: Optional[Dict] = None):
         """预测批次开始的时候"""
         pass
 
+    @empty_implementation
     def on_predict_batch_end(self, batch: int, logs: Optional[Dict] = None):
         """预测批次结束的时候"""
         pass
